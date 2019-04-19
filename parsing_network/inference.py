40f088e3f9de4177d586862cee3174e36a2e76da
Andrew Jong <andrewjong87@gmail.com>
2019-04-19 10:58:32 -0700

    Improve inference script for my needs
    arguments reflect structure of my data folders
    save visualized mask in addition to compressed npz form
    check if rois.csv file already exists

--- parsing_network/inference.py
+++ parsing_network/inference.py
@@ -6,6 +6,7 @@ This script computes a segmentation mask for a given image.
 import argparse
 import os
 import time
+import sys
 
 import numpy as np
 import tensorflow as tf
@@ -17,8 +18,6 @@ from tqdm import trange
 IMG_MEAN = np.array((104.00698793, 116.66876762, 122.67891434), dtype=np.float32)
 
 NUM_CLASSES = 7
-DATA_LIST = "./dataset/dance.txt"
-SAVE_DIR = "./output/"
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
@@ -29,22 +28,47 @@ def get_arguments():
     Returns:
       A list of parsed arguments.
     """
-    parser = argparse.ArgumentParser(description="DeepLabLFOV Network Inference.")
-    parser.add_argument("img_path", type=str, help="Path to the RGB image file folder.")
-    parser.add_argument(
-        "model_weights", type=str, help="Path to the file with model weights."
+    parser = argparse.ArgumentParser(
+        description="Body segmentation inference, customized for andrew's work",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        "--data_list", type=str, default=DATA_LIST, help="Path to the image list."
+        "img_path",
+        type=str,
+        help="Path to the top level data directory, containing a 'texture' subfolder.",
     )
     parser.add_argument(
-        "--num-classes",
+        "--data_list", default="list.txt", help="Path to the image list relative to img_path"
+    )
+    parser.add_argument(
+        "--body_dir",
+        type=str,
+        default="body",
+        help="Where to save body mask relative to img_path.",
+    )
+    parser.add_argument(
+        "--vis_dir",
+        type=str,
+        default="body_vis",
+        help="Where to save visualized mask relative to img_path.",
+    )
+    parser.add_argument(
+        "--visualize_step",
+        type=int,
+        default=1000,
+        help="how often in steps to visualize the mask",
+    )
+    parser.add_argument(
+        "--model_weights",
+        default="models/final_model/model.ckpt-19315",
+        type=str,
+        help="Path to the file with model weights.",
+    )
+    parser.add_argument(
+        "--num_classes",
         type=int,
         default=NUM_CLASSES,
         help="Number of classes to predict (including background).",
-    )
-    parser.add_argument(
-        "--save-dir", type=str, default=SAVE_DIR, help="Where to save predicted mask."
     )
     return parser.parse_args()
 
@@ -68,18 +92,31 @@ def file_len(fname):
     return i + 1
 
 
+def write_header(rois_file):
+    with open(rois_file, "w") as f:
+        f.write("id,x1,y1,x2,y2\n")
+
+
+# AJ function
+def extract_nums_only(frame_name):
+    as_str = "".join(filter(lambda x: x.isdigit(), frame_name))
+    # strip leading 0s
+    as_str = as_str.lstrip("0")
+    return as_str
+
+
 def main():
     """Create the model and start the evaluation process."""
     args = get_arguments()
-    num_steps = file_len(args.data_list)
+    num_steps = file_len(os.path.join(args.img_path, args.data_list))
     # Create queue coordinator.
     coord = tf.train.Coordinator()
 
     # Load reader.
     with tf.name_scope("create_inputs"):
         reader = ImageReader(
-            args.img_path,
-            args.data_list,
+            os.path.join(args.img_path, "texture"),
+            os.path.join(args.img_path, args.data_list),
             None,  # No defined input size.
             False,  # No random scale.
             False,  # No random mirror.
@@ -132,13 +169,24 @@ def main():
     threads = tf.train.start_queue_runners(coord=coord, sess=sess)
 
     start_time = time.time()
-    if not os.path.exists(args.save_dir):
-        os.makedirs(args.save_dir)
+    os.makedirs(os.path.join(args.img_path, args.body_dir), exist_ok=True)
+    os.makedirs(os.path.join(args.img_path, args.vis_dir), exist_ok=True)
 
     # write the header
-    rois_file = os.path.join(args.save_dir, "rois.csv")
-    with open(rois_file, "w") as f:
-        f.write("id,x1,y1,x2,y2\n")
+    rois_file = os.path.join(args.img_path, "rois.csv")
+    if os.path.isfile(rois_file):
+        print(f"The rois file {rois_file} already exists...")
+        ans = None
+        while all(ans != choice for choice in ("a", "o", "q")):
+            ans = input("Do you want to (a)ppend, (o)verwrite, or (q)uit? ")
+        if ans == "o":
+            print("Overwriting existing rois file...")
+            write_header(rois_file)
+        elif ans == "q":
+            sys.exit(1)
+    else:
+        write_header(rois_file)
+
     # Perform inference.
     t = trange(num_steps, desc="Inference progress", unit="img")
     for step in t:
@@ -158,15 +206,16 @@ def main():
         )
 
         # == First, save the body segmentation ==
-        # convert to a compressed matrix, because we have a lot of 0's for the
+        # convert to a 2D compressed matrix, because we have a lot of 0's for the
         # background
         compressed = sparse.csr_matrix(np.squeeze(raw_output_up_))
         fname = os.path.splitext(os.path.basename(str(jpg_path)))[0]
-        out = os.path.join(args.save_dir, "body", fname)
+        out = os.path.join(args.img_path, args.body_dir, fname)
         sparse.save_npz(out, compressed)
 
         # == Next, save the ROIs ==
 
+        img_id = extract_nums_only(fname)
         for c in (c0, c1, c2, c3, c4, c5, c6):
             try:
                 min_x = np.min(c[:, 1])
@@ -188,36 +237,35 @@ def main():
             with open(rois_file, "a") as f:
                 f.write(
                     ",".join(
-                        (str(step), str(min_x), str(min_y), str(max_x), str(max_y),
-                         "\n")
+                        (img_id, str(min_x), str(min_y), str(max_x), str(max_y), "\n")
                     )
                 )
 
-        # msk = decode_labels(preds, num_classes=args.num_classes)
-        # # the mask
-        # im = Image.fromarray(msk[0])
-        # # save the mask
-        # jpg_path = str(jpg_path).split('/')[-1].split('.')[0]
-        # out = os.path.join(args.save_dir, jpg_path + '.png')
-        # im.save(out)
+        # Save an image of the mask for our own reference every 1000 steps
+        if step % args.visualize_step == 0:
+            preds = np.expand_dims(raw_output_up_, axis=3)
+            msk = decode_labels(preds, num_classes=args.num_classes)
+            # the mask
+            im = Image.fromarray(msk[0])
+            # # Save the mask separately
+            # jpg_path = str(jpg_path).split('/')[-1].split('.')[0]
+            # out = os.path.join(args.vis_dir, jpg_path + '.png')
+            # im.save(out)
+            # Save the mask with background
+            img_orig = Image.open(jpg_path)
+            # create the final result using the mask and the original
+            img = np.array(im) * 0.9 + np.array(img_orig) * 0.7
+            # clip surpassed colors
+            img[img > 255] = 255
+            img = Image.fromarray(np.uint8(img))
+            out = os.path.join(args.img_path, args.vis_dir, fname + ".png")
+            img.save(out)
+            # # print('Image processed {}.png'.format(jpg_path))
         t.set_description("Finished " + fname)
 
-        # AJ: We want to save only the mask, not the background. therefore we commented out below
-
-        # # the original image
-        # img_o = Image.open(jpg_path)
-        # jpg_path = jpg_path.split('/')[-1].split('.')[0]
-        # # create the final result using the mask and the original
-        # img = np.array(im)*0.9 + np.array(img_o)*0.7
-        # # clip surpassed colors
-        # img[img>255] = 255
-        # img = Image.fromarray(np.uint8(img))
-        # img.save(args.save_dir + jpg_path + '.png')
-        # # print('Image processed {}.png'.format(jpg_path))
-
     total_time = time.time() - start_time
-    print(("The output files have been saved to {}".format(args.save_dir)))
-    print(("It took {} sec on each image.".format(total_time / num_steps)))
+    print(f"The output files have been saved to {args.img_path}/{args.body_dir}")
+    print(f"It took {total_time / num_steps} sec on each image.")
 
 
 if __name__ == "__main__":
